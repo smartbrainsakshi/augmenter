4ff0d66b5b4f22a729eeff3723fd6b407b0087e8
dPSudocoder <princesharzeel.10@gmail.com>
2021-07-19 02:22:10 +0530

    :zap: psql

--- augmenter/settings.py
+++ augmenter/settings.py
@@ -83,12 +83,15 @@ WSGI_APPLICATION = 'augmenter.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "augementer_db",
+        "USER": "augmenter",
+        "PASSWORD": "pass321",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
